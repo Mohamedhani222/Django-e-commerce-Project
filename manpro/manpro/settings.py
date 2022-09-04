f8db494fdc49773c4167df6cb9c0c4dcd870d110
Dev / Mohamed Hani <112791548+Mohamedhani222@users.noreply.github.com>
2022-09-04 06:32:02 +0200

    Update settings.py

--- manpro/manpro/settings.py
+++ manpro/manpro/settings.py
@@ -149,9 +149,3 @@ MEDIA_URL = 'media/'
 
 
 
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_HOST_USER='mohamedhacker740@gmail.com'
-EMAIL_HOST_PASSWORD='koiashaqejzrdshu'
-EMAIL_USE_TLS= True
-EMAIL_PORT='587'
